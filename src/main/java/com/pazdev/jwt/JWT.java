c1f68cda4aeca12915e2d0c5a091cacc94c15413
Jonathan <Jonathan@paztop>
2016-11-24 22:39:57 -0600

    Initial commit

---
+++ src/main/java/com/pazdev/jwt/JWT.java
@@ -0,0 +1,329 @@
+/*
+ * Copyright 2016 Jonathan Paz <jonathan@pazdev.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pazdev.jwt;
+
+import com.pazdev.jose.Algorithm;
+import com.pazdev.jose.Header;
+import com.pazdev.jose.JWE;
+import com.pazdev.jose.JWK;
+import com.pazdev.jose.JWS;
+import java.nio.charset.StandardCharsets;
+import java.security.Key;
+import java.util.Base64;
+
+/**
+ *
+ * @author Jonathan Paz <jonathan@pazdev.com>
+ */
+public final class JWT {
+    public static final class Builder {
+        private JWTClaims claims;
+        private Algorithm signingAlgorithm;
+        private JWK verifyingKeyJWT;
+        private JWK encryptionKeyJWT;
+        private String verifyingKeyId;
+        private String encryptionKeyId;
+        private Algorithm keyManagementAlgorithm;
+        private Algorithm encryptionAlgorithm;
+        private Key signingKey;
+        private Key encryptionKey;
+        private boolean specifyType = false;
+        private boolean issuerInJWEHeader = false;
+        private boolean subjectInJWEHeader = false;
+        private boolean audienceInJWEHeader = false;
+
+        public Builder withClaims(JWTClaims claims) {
+            this.claims = claims;
+            return this;
+        }
+
+        public Builder withSigningAlgorithm(Algorithm signingAlgorithm) {
+            this.signingAlgorithm = signingAlgorithm;
+            return this;
+        }
+
+        public Builder withVerifyingKeyJWT(JWK verifyingKeyJWT) {
+            this.verifyingKeyJWT = verifyingKeyJWT;
+            return this;
+        }
+
+        public Builder withEncryptionKeyJWT(JWK encryptionKeyJWT) {
+            this.encryptionKeyJWT = encryptionKeyJWT;
+            return this;
+        }
+
+        public Builder withEncryptionKeyId(String id) {
+            this.encryptionKeyId = id;
+            return this;
+        }
+
+        public Builder withVerifyingKeyId(String id) {
+            this.verifyingKeyId = id;
+            return this;
+        }
+        public Builder withKeyManagementAlgorithm(Algorithm keyManagementAlgorithm) {
+            this.keyManagementAlgorithm = keyManagementAlgorithm;
+            return this;
+        }
+
+        public Builder withEncryptionAlgorithm(Algorithm encryptionAlgorithm) {
+            this.encryptionAlgorithm = encryptionAlgorithm;
+            return this;
+        }
+
+        public Builder withSigningKey(Key signingKey) {
+            this.signingKey = signingKey;
+            return this;
+        }
+
+        public Builder withEncryptionKey(Key encryptionKey) {
+            this.encryptionKey = encryptionKey;
+            return this;
+        }
+
+        public void withSpecifyType(boolean specifyType) {
+            this.specifyType = specifyType;
+        }
+
+        public void withIssuerInJWEHeader(boolean issuerInJWEHeader) {
+            this.issuerInJWEHeader = issuerInJWEHeader;
+        }
+
+        public void withSubjectInJWEHeader(boolean subjectInJWEHeader) {
+            this.subjectInJWEHeader = subjectInJWEHeader;
+        }
+
+        public void withAudienceInJWEHeader(boolean audienceInJWEHeader) {
+            this.audienceInJWEHeader = audienceInJWEHeader;
+        }
+
+        public String build() {
+            String retval = null;
+            if (encryptionAlgorithm != null ^ encryptionKey != null) {
+                throw new IllegalStateException("Both encryptionKey and encryptionAlgorithm must be set");
+            }
+            if (signingAlgorithm != null ^ signingKey != null) {
+                throw new IllegalStateException("Both signingKey and signingAlgorithm must be set");
+            }
+            if (signingKey != null) {
+                Header jwsHeader = Header.builder()
+                        .withAlgorithm(signingAlgorithm)
+                        .withJsonWebKey(verifyingKeyJWT)
+                        .withKeyId(verifyingKeyId)
+                        .withType(specifyType ? "JWT" : null)
+                        .build();
+                retval = JWS.builder()
+                        .withSignature(jwsHeader, null, signingKey)
+                        .withPayload(claims)
+                        .build()
+                        .toCompact();
+            }
+            if (encryptionKey != null) {
+                String payload;
+                String cty = null;
+                if (retval != null) {
+                    payload = retval;
+                    cty = "JWT";
+                } else {
+                    payload = claims.toJson();
+                }
+                Header jweHeader = Header.builder()
+                        .withAlgorithm(keyManagementAlgorithm)
+                        .withEncryptionAlgorithm(encryptionAlgorithm)
+                        .withJsonWebKey(encryptionKeyJWT)
+                        .withKeyId(encryptionKeyId)
+                        .withContentType(cty)
+                        .withType(specifyType ? "JWT" : null)
+                        .withIssuer(issuerInJWEHeader ? claims.getIssuer() : null)
+                        .withSubject(subjectInJWEHeader ? claims.getSubject() : null)
+                        .withAudience(audienceInJWEHeader ? claims.getAudience() : null)
+                        .build();
+                retval = JWE.builder()
+                        .withKey(encryptionKey)
+                        .withProtectedHeader(jweHeader)
+                        .withPayload(payload)
+                        .build()
+                        .toCompact();
+            }
+            if (retval == null) {
+                Header header = Header.builder().withAlgorithm(Algorithm.NONE).build();
+                Base64.Encoder encoder = Base64.getUrlEncoder().withoutPadding();
+                retval = String.format("%s.%s.",
+                        encoder.encodeToString(header.toJson().getBytes(StandardCharsets.UTF_8)),
+                        encoder.encodeToString(claims.toJson().getBytes(StandardCharsets.UTF_8)));
+            }
+            return retval;
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public static JWTClaims verify(String token, Key encryptionKey, Key verificationKey) {
+        JWTClaims retval;
+        // step 1
+        if (!token.contains(".")) {
+            throw new IllegalArgumentException("Malformatted token");
+        }
+
+        // step 2
+        String[] parts = token.split("\\.");
+
+        // step 3
+        byte[] headerbytes = Base64.getUrlDecoder().decode(parts[0]);
+
+        // step 4
+        Header header = Header.parse(new String(headerbytes, StandardCharsets.UTF_8));
+        
+        // step 5
+        if (header.getCritical() != null) {
+            throw new IllegalArgumentException("Critical arguments not supported");
+        }
+        // the rest of step 5 happens during the next step
+        // step 6
+        switch (parts.length) {
+            case 5: {
+                // JWE
+                if (encryptionKey == null) {
+                    throw new IllegalArgumentException("encryption key is required");
+                }
+                String payload = JWE.parse(token).decrypt(encryptionKey, StandardCharsets.UTF_8);
+                if ("JWT".equals(header.getContentType())) {
+                    // encrypted signed-JWT
+                    retval = verify(payload, verificationKey);
+                } else {
+                    retval = JWTClaims.parse(payload);
+                }
+                break;
+            }
+            case 3: {
+                // JWS
+                if (verificationKey == null) {
+                    JWK jwk = header.getJsonWebKey();
+                    verificationKey = jwk.getKeys().get("public");
+                    if (verificationKey == null) {
+                        throw new IllegalArgumentException("public key cannot be found and is not specified");
+                    }
+                }
+                JWS jws = JWS.parse(token);
+                String payload = jws.getPayload(StandardCharsets.UTF_8);
+                if (!jws.verify(verificationKey)) {
+                    throw new IllegalArgumentException("Signature does not verify");
+                }
+                if ("JWT".equals(header.getContentType())) {
+                    // signed encrypted-JWT
+                    retval = verify(payload, encryptionKey);
+                } else {
+                    retval = JWTClaims.parse(payload);
+                }
+                break;
+            }
+            default:
+                throw new IllegalArgumentException("Unparseable JWT");
+        }
+        return retval;
+    }
+
+    public static JWTClaims verify(String token, Key key) {
+        JWTClaims retval;
+        // step 1
+        if (!token.contains(".")) {
+            throw new IllegalArgumentException("Malformatted token");
+        }
+
+        // step 2
+        String[] parts = token.split("\\.");
+
+        // step 3
+        byte[] headerbytes = Base64.getUrlDecoder().decode(parts[0]);
+
+        // step 4
+        Header header = Header.parse(new String(headerbytes, StandardCharsets.UTF_8));
+        
+        // step 5
+        if (header.getCritical() != null) {
+            throw new IllegalArgumentException("Critical arguments not supported");
+        }
+        // the rest of step 5 happens during the next step
+        // step 6
+        switch (parts.length) {
+            case 5: {
+                // JWE
+                if (key == null) {
+                    throw new IllegalArgumentException("encryption key is required");
+                }
+                String payload = JWE.parse(token).decrypt(key, StandardCharsets.UTF_8);
+                if ("JWT".equals(header.getContentType())) {
+                    // encrypted signed-JWT
+                    retval = verify(payload, null);
+                } else {
+                    retval = JWTClaims.parse(payload);
+                }
+                break;
+            }
+            case 3: {
+                // JWE
+                Key vkey;
+                Key ekey;
+                JWK jwk = header.getJsonWebKey();
+                if (jwk != null) {
+                    vkey = jwk.getKeys().get("public");
+                    if (vkey == null) {
+                        if (key == null) {
+                            throw new IllegalArgumentException("public key cannot be found and is not specified");
+                        } else {
+                            vkey = key;
+                            ekey = null;
+                        }
+                    } else {
+                        ekey = key;
+                    }
+                } else {
+                    vkey = key;
+                    ekey = null;
+                }
+                JWS jws = JWS.parse(token);
+                String payload = jws.getPayload(StandardCharsets.UTF_8);
+                if (!jws.verify(vkey)) {
+                    throw new IllegalArgumentException("Signature does not verify");
+                }
+                if ("JWT".equals(header.getContentType())) {
+                    // signed encrypted-JWT
+                    if (ekey != null) {
+                        retval = verify(payload, ekey);
+                    } else {
+                        throw new UnsupportedOperationException("Encryption key is missing");
+                    }
+                } else {
+                    retval = JWTClaims.parse(payload);
+                }
+                break;
+            }
+            default:
+                throw new IllegalArgumentException("Unparseable JWT");
+        }
+        return retval;
+    }
+
+    public static JWTClaims verify(String token) {
+        return verify(token, null);
+    }
+
+    private JWT() {
+        throw new UnsupportedOperationException("Uninstantiatable class");
+    }
+}
---
+++ src/main/java/com/pazdev/jwt/JWTClaims.java
@@ -0,0 +1,200 @@
+/*
+ * Copyright 2016 Jonathan Paz <jonathan@pazdev.com>.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.pazdev.jwt;
+
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.Base64Variants;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import com.google.common.collect.ImmutableMap;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Jonathan Paz <jonathan@pazdev.com>
+ */
+@JsonDeserialize(builder = JWTClaims.Builder.class)
+public final class JWTClaims {
+    @JsonProperty("iss")
+    private final String issuer;
+    @JsonProperty("sub")
+    private final String subject;
+    @JsonProperty("aud")
+    private final String audience;
+    @JsonProperty("exp")
+    private final Instant expirationTime;
+    @JsonProperty("nbf")
+    private final Instant notBefore;
+    @JsonProperty("iat")
+    private final Instant issuedAt;
+    @JsonProperty("jti")
+    private final String jwtId;
+    private final Map<String, Object> claims;
+
+    private JWTClaims(String issuer, String subject, String audience, Instant expirationTime, Instant notBefore, Instant issuedAt, String jwtId , Map<String, Object> claims) {
+        this.issuer = issuer;
+        this.subject = subject;
+        this.audience = audience;
+        this.expirationTime = expirationTime;
+        this.notBefore = notBefore;
+        this.issuedAt = issuedAt;
+        this.jwtId = jwtId;
+        this.claims = claims;
+    }
+
+    @JsonPOJOBuilder
+    public static final class Builder {
+        private String issuer;
+        private String subject;
+        private String audience;
+        private Instant expirationTime;
+        private Instant notBefore;
+        private Instant issuedAt;
+        private String jwtId;
+        private final HashMap<String, Object> claims = new HashMap<>();
+
+        @JsonProperty("iss")
+        public Builder withIssuer(String issuer) {
+            this.issuer = issuer;
+            return this;
+        }
+
+        @JsonProperty("sub")
+        public Builder withSubject(String subject) {
+            this.subject = subject;
+            return this;
+        }
+
+        @JsonProperty("aud")
+        public Builder withAudience(String audience) {
+            this.audience = audience;
+            return this;
+        }
+
+        @JsonProperty("exp")
+        public Builder withExpirationTime(Instant expirationTime) {
+            this.expirationTime = expirationTime;
+            return this;
+        }
+
+        @JsonProperty("nbf")
+        public Builder withNotBefore(Instant notBefore) {
+            this.notBefore = notBefore;
+            return this;
+        }
+
+        @JsonProperty("iat")
+        public Builder withIssuedAt(Instant issuedAt) {
+            this.issuedAt = issuedAt;
+            return this;
+        }
+
+        @JsonProperty("jti")
+        public Builder withJwtId(String jwtId) {
+            this.jwtId = jwtId;
+            return this;
+        }
+
+        @JsonAnySetter
+        public Builder withClaim(String key, Object value) {
+            this.claims.put(key, value);
+            return this;
+        }
+
+        public Builder withClaims(Map<String, Object> claims) {
+            if (claims != null) {
+                this.claims.putAll(claims);
+            }
+            return this;
+        }
+        
+        public JWTClaims build() {
+            return new JWTClaims(issuer, subject, audience, expirationTime, notBefore, issuedAt, jwtId, ImmutableMap.copyOf(claims));
+        }
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public String getIssuer() {
+        return issuer;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public String getAudience() {
+        return audience;
+    }
+
+    public Instant getExpirationTime() {
+        return expirationTime;
+    }
+
+    public Instant getNotBefore() {
+        return notBefore;
+    }
+
+    public Instant getIssuedAt() {
+        return issuedAt;
+    }
+
+    public String getJwtId() {
+        return jwtId;
+    }
+
+    @JsonAnyGetter
+    public Map<String, Object> getClaims() {
+        return claims;
+    }
+
+    public Object getClaim(String key) {
+        return claims.get(key);
+    }
+
+    public <T> T getClaim(String key, Class<T> cls) {
+        return cls.cast(claims.get(key));
+    }
+
+    public String toJson() {
+        ObjectMapper om = new ObjectMapper();
+        om.setBase64Variant(Base64Variants.MODIFIED_FOR_URL);
+        try {
+            return om.writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static JWTClaims parse(String json) {
+        ObjectMapper om = new ObjectMapper();
+        om.setBase64Variant(Base64Variants.MODIFIED_FOR_URL);
+        try {
+            return om.readValue(json, JWTClaims.class);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
